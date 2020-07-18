b28028ce9cfed81ad3682d4bae057a618577b288
Sergio Londoño <sergio.londonog@hotmail.com>
2020-07-18 11:12:22 -0500

    Unit test Project Added.

---
+++ UnitTestMasGlobalService/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestMasGlobalService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestMasGlobalService")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db5da016-9c12-46e5-b81d-c27e6506bb0f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestMasGlobalService/UnitTestDomain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MasGlobalService.DTO;
+using MasGlobalService.Domain;
+using MasGlobalService.Factories;
+using System.Linq;
+
+namespace UnitTestMasGlobalService
+{
+    [TestClass]
+    public class UnitTestDomain
+    {
+        [TestMethod]
+        public void getCalculatedSalaryByEmployee_WithEmployeeHourlySalary_Return120XHourSalaryX12()
+        {
+            // Arrange
+            List<EmployeeDTO> employees = new List<EmployeeDTO>() {
+            new EmployeeDTO(){ id = 1, name = "Juan",
+                               contractTypeName = "HourlySalaryEmployee",  roleId= 1,
+                               roleName= "Administrator", roleDescription= "",
+                               hourlySalary= 35000, monthlySalary= 80000 }
+            };
+
+            EmployeeDomain domain = new EmployeeDomain();
+            SalaryFactory factory = new ConcreteSalaryFactory();
+
+            // Act
+            var calculatedEmployeeSalary = domain.getCalculatedSalaryByEmployee(employees, factory).ToList();
+
+            decimal actual = calculatedEmployeeSalary[0].AnnualSalary;
+
+            // Assert
+            decimal expected = 50400000;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void getCalculatedSalaryByEmployee_WithEmployeeMonthlySalary_ReturnMonthlySalaryX12()
+        {
+            // Arrange
+            List<EmployeeDTO> employees = new List<EmployeeDTO>() {
+            new EmployeeDTO(){ id = 2, name = "Pedro",
+                               contractTypeName = "MonthlySalaryEmployee",  roleId= 1,
+                               roleName= "Administrator", roleDescription= "",
+                               hourlySalary= 50000, monthlySalary= 400000 }
+            };
+
+            EmployeeDomain domain = new EmployeeDomain();
+            SalaryFactory factory = new ConcreteSalaryFactory();
+
+            // Act
+            var calculatedEmployeeSalary = domain.getCalculatedSalaryByEmployee(employees, factory).ToList();
+
+            decimal actual = calculatedEmployeeSalary[0].AnnualSalary;
+
+            // Assert
+            decimal expected = 4800000;
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
